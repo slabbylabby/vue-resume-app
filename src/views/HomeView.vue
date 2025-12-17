2271c70b15331a93d4c057bf0dc40585d9defa77
slabbylabby <sethbtheog@gmail.com>
2025-12-17 12:58:53 -0500

    more redundant css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -84,73 +84,25 @@
             @click="setActiveSection('about')"
             :class="['tab-button btn', { active: activeSection === 'about' }]"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-              <path
-                d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"
-              />
-            </svg>
-            About
+            <span>About</span>
           </button>
           <button
             @click="setActiveSection('experience')"
             :class="['tab-button btn', { active: activeSection === 'experience' }]"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M5 2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2h3a1 1 0 0 1 1 1v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a1 1 0 0 1 1-1h3V2zm6 2h-1V2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2H5v8a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4z"
-              />
-            </svg>
-            Experience
+            <span>Experience</span>
           </button>
           <button
             @click="setActiveSection('education')"
             :class="['tab-button btn', { active: activeSection === 'education' }]"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M4.5 13.5A1.5 1.5 0 0 0 6 15h4a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5H6a.5.5 0 0 1-.5-.5v-6a.5.5 0 0 0-1 0v6z"
-              />
-              <path
-                d="M8.648.711A.5.5 0 0 1 9 .5l.5-.5.5.5a.5.5 0 0 1 .352.711L8.648.711zm.5-.25a.5.5 0 0 0-.5 0l-5 2.5v2.5a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-2.5l-5-2.5z"
-              />
-            </svg>
-            Education
+            <span>Education</span>
           </button>
           <button
             @click="setActiveSection('portfolio')"
             :class="['tab-button btn', { active: activeSection === 'portfolio' }]"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"
-              />
-            </svg>
-            Portfolio
+            <span>Portfolio</span>
           </button>
         </nav>
 
